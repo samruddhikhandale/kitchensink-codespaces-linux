6d2156cafac44b23aabb42f529e1aa8c4548785d
Samruddhi Khandale <khandale.samruddhi@gmail.com>
2021-07-28 20:41:50 +0000

    python

--- checklist.service/index.js
+++
@@ -1,7 +0,0 @@
-const server = require('./server.js')
-const process = require('process');
-
-const APP_PORT = process.env.PORT || 8080
-
-// Start Server
-server.listen(APP_PORT, () => console.log(`Server started on ${APP_PORT}`));
